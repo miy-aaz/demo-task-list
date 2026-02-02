60696b325cfe29c0fca6c07f12f734071b11f4df
AZIZ <Miya.AZIZ@EDUCATION.GOV.UK>
2026-02-02 16:41:54 +0000

    added eligibility flow

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,27 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+router.post('/has-account', (req, res) => {
+   if(req.session.data.new.hasAccount == 'No') {
+    res.redirect('/can-juggle')
+   } else {
+    res.redirect('/email-address')
+   }
+})
+
+router.post('/can-juggle', (req, res) => {
+   if(req.session.data.new.canJuggle == 'No') {
+    res.redirect('/not-eligible')
+   } else {
+    res.redirect('/email-address')
+   }
+})
+
+router.post('/email-address', (req, res) => {
+    res.redirect('/code')
+})
+
+router.post('/code', (req, res) => {
+    res.redirect('/application')
+})
